a5ac5ef1da89c556e17860089d9dd02f1f606047
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-07-21 10:54:17 +0700

    insert

--- nodeJS_s4/index.js
+++ nodeJS_s4/index.js
@@ -4,8 +4,30 @@ const url = "mongodb://localhost:27017"
 const dbClient = new MongoClient(url)
 dbClient.connect(() => {
     console.log("DB connected")
+    const db = dbClient.db('mindx')
+    // db.collection("mindx").insertOne({
+    //     name: 'A',
+    //     age:20,
+    //     address: "SG"
+    // })
+    // db.collection("mindx").insertMany([
+    //     {
+    //         name: 'A',
+    //         age:20,
+    //         address: "SG"
+    //     },
+    //     {
+    //         name: 'A',
+    //         age:20,
+    //         address: "SG"
+    //     },
+    //     {
+    //         name: 'A',
+    //         age:20,
+    //         address: "SG"
+    //     }
+    // ])
 })
-
 const app = express()
 
 app.listen(5001, () => {
