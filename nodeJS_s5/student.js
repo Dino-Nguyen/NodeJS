73294c7389e1ed4ad69cfb278d24c129a04bbf3c
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-07-21 12:47:21 +0700

    get obj id

--- nodeJS_s5/student.js
+++ nodeJS_s5/student.js
@@ -1,9 +1,19 @@
 const express = require("express")
+const {ObjectId} = require("mongodb")
 const studentRouter = express.Router()
 const {db} = require('./db')
 
-studentRouter.get("/", (req, res) => {})
-studentRouter.get("/:id", (req, res) => {})
+studentRouter.get("/", async(req, res) => {
+    const student = await db.student.find().toArray()
+    res.json(student)
+})
+studentRouter.get("/:id", async (req, res) => {
+    const id = req.params.id
+    const student =await db.student.find({
+        _id: ObjectId(id),
+    }).toArray()
+    res.json(student)
+})
 studentRouter.post("/", async (req, res) => {
     const student ={
         name: req.body.name,
@@ -12,6 +22,10 @@ studentRouter.post("/", async (req, res) => {
     }
   const result = await db.student.insertOne(student)
     console.log(result)
+    res.json({
+        _id : result.insertedId,
+        student : student
+    })
 })
 studentRouter.put("/:id", (req, res) => {})
 studentRouter.delete("/:id", (req, res) => {})
