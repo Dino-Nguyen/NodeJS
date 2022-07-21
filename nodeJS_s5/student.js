9e1dc3c5930972965d332eed14d8ec62b5d277ed
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-07-21 12:36:42 +0700

    async

--- nodeJS_s5/student.js
+++ nodeJS_s5/student.js
@@ -4,14 +4,14 @@ const {db} = require('./db')
 
 studentRouter.get("/", (req, res) => {})
 studentRouter.get("/:id", (req, res) => {})
-studentRouter.post("/", (req, res) => {
+studentRouter.post("/", async (req, res) => {
     const student ={
         name: req.body.name,
         gender : req.body.gender,
         dob : new Date(req.body.dob),
     }
-    db.student.insertOne(student)
-    
+  const result = await db.student.insertOne(student)
+    console.log(result)
 })
 studentRouter.put("/:id", (req, res) => {})
 studentRouter.delete("/:id", (req, res) => {})
